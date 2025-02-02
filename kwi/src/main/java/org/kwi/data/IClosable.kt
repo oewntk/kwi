279e8b172d9d9c1401c1c123c5c86aea3ebfb011
1313ou <1313ou@gmail.com>
2025-02-02 07:57:25 +0100

    copyright

--- kwi/src/main/java/org/kwi/data/IClosable.kt
+++ kwi/src/main/java/org/kwi/data/IClosable.kt
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2025.
+ * Java Wordnet Interface Library (JWI) v2.4.0 Copyright (c) 2007-2015 Mark A. Finlayson
+ * Kotlin Wordnet Interface Library (KWI) v1.0 Copyright (c) 2025 Bernard Bou
+ * JWI is distributed under the terms of the Creative Commons Attribution 4.0 International Public License
+ * KWI is distributed under the terms of the GPL3 License
+ */
+
 package org.kwi.data
 
 /**
--- kwi/src/main/java/org/kwi/item/Gloss.kt
+++ kwi/src/main/java/org/kwi/item/Gloss.kt
@@ -1,5 +1,9 @@
 /*
- * Copyright (c) 2021-2024. Bernard Bou.
+ * Copyright (c) 2021-2025.
+ * Java Wordnet Interface Library (JWI) v2.4.0 Copyright (c) 2007-2015 Mark A. Finlayson
+ * Kotlin Wordnet Interface Library (KWI) v1.0 Copyright (c) 2025 Bernard Bou
+ * JWI is distributed under the terms of the Creative Commons Attribution 4.0 International Public License
+ * KWI is distributed under the terms of the GPL3 License
  */
 package org.kwi.item
 
